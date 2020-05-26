7975212df6095e73a2c00a739ae9aa1aa3cdf111
novastosha <galliigotu@gmail.com>
2020-05-26 20:31:37 +0100

    Added:  - Added PrivateMessageReceiveEvent class (Called when a player receives a message)

---
+++ main/java/events/PrivateMessageReceiveEvent.java
@@ -0,0 +1,48 @@
+package events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PrivateMessageReceiveEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private boolean canceled;
+
+    private Player from;
+    private Player to;
+    private String message;
+
+    public Player getTo() {
+        return to;
+    }
+
+    public Player getFrom() {
+        return from;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return canceled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.canceled = b;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+}
