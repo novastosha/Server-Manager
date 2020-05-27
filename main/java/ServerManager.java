dfbdc90fb66720d1d9733823e76c6c18286c954c
diniamo <diniamo69@gmail.com>
2020-05-27 13:14:17 +0200

    Fixed some merge shit

--- main/java/ServerManager.java
+++ main/java/ServerManager.java
@@ -8,11 +8,8 @@ public class ServerManager extends JavaPlugin {
     public static long time;
     @Override
     public void onEnable() {
-        getCommand("gmc").setExecutor(new GamemodeCommand());
-        getCommand("gms").setExecutor(new GamemodeCommand());
-        getCommand("gma").setExecutor(new GamemodeCommand());
-        getCommand("gmp").setExecutor(new GamemodeCommand());
         Bukkit.getPluginManager().registerEvents(new DMListener(),this);
+
         GamemodeCommand gamemodeCommand = new GamemodeCommand();
         getCommand("gamemode").setExecutor(gamemodeCommand);
         getCommand("gamemode").setTabCompleter(gamemodeCommand);
